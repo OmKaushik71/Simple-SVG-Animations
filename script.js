86fb2d221ac960f8a7d220eaf5928f4775192117
Om Kaushik <omkaushik015@gmail.com>
2022-12-24 15:11:01 +0530

    improved loading animation

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 window.addEventListener("DOMContentLoaded", () => {
   const tickPath = document.querySelector("#tick path");
   console.log(tickPath.getTotalLength());
+
   const downloadPath = document.querySelector("#download path");
   console.log(downloadPath.getTotalLength());
 
-  function pageScroll() {
-    window.scrollBy(0, 1);
-    scrolldelay = setTimeout(pageScroll, 3);
-  }
+  const rotatePath = document.querySelector("#rotate path");
+  console.log(rotatePath.getTotalLength());
   function bottomScroll() {
     window.scrollTo(0, document.body.scrollHeight);
     // scrolldelay = setTimeout(bottomScroll, 10);
